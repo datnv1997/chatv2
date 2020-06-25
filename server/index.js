9229a24890977d94f3fabdef27e2f1363375d2f7
datnv1997 <datnv1997@gmail.com>
2020-06-25 16:50:12 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 var port = process.env.PORT || 3030;
 
-app.get('/', function (req, res) {
-  res.sendFile('/public/index.html');
-});
+// app.get("/", function (req, res) {
+//   res.sendFile("/public/index.html");
+// });
 
-io.on('connection', function (socket) {
-  io.emit('dataSocket', {
+io.on("connection", function (socket) {
+  io.emit("dataSocket", {
     totalClient: socket.client.conn.server.clientsCount,
-    socketId: socket.id
+    socketId: socket.id,
   });
-
+  socket.on("all client", function (data) {
+    console.log(data);
+    io.emit("Message", data); // gửi cho tất cả client
+    // console.log(data);
+  });
 });
 
 http.listen(port, function () {
-  console.log('listening on *:' + port);
+  console.log("listening on *:" + port);
 });
